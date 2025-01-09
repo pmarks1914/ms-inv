2ba33432da502dbc7650867f182bd671d735eea6
Patrick <pmarks1914@gmail.com>
2025-01-09 23:55:53 +0000

    inv

--- fileManager/fileManager.py
+++ fileManager/fileManager.py
@@ -53,11 +53,14 @@ def fileUploadManager(request, user_id, user_email, *args):
             s3_object_name = new_filename
             # Upload the file to S3
             try:
-                s3.upload_file(local_file_path, bucket_name, s3_object_name, 
-                ExtraArgs={
-                    'ContentType': str(file_type),
-                    'ContentDisposition': 'inline'
-                } )
+                # s3.upload_file(local_file_path, bucket_name, s3_object_name, 
+                # ExtraArgs={
+                #     'ContentType': str(file_type),
+                #     'ContentDisposition': 'inline'
+                # } )
+
+                azure_blob(new_filename, local_file_path, str(file_type))
+
                 os.remove(local_file_path)  # Clean up the local file after upload
                 return {'message': 'File uploaded successfull', 'status': True}
             except Exception as e:
@@ -105,4 +108,40 @@ def fileUploadManager(request, user_id, user_email, *args):
 
     except Exception as e:
         return {'message': 'File uploading failed', 'status': False, 'error': str(e)}
+
+
+
+def azure_blob(blob_name, file_path, file_type):
+    # Set up your connection string and container name
+    connection_string = get_env['AZURE_CONNECTION_STRING'] 
+    container_name = get_env['AZURE_CONTAINER_NAME'] 
+    # Create a BlobServiceClient object using the connection string
+    try:
+        # Create a BlobServiceClient object using the connection string
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        # Create a container client
+        container_client = blob_service_client.get_container_client(container_name)
+        # Check if the container exists
+        if not container_client.exists():
+            # container_client.create_container()
+            print(f"Container '{container_name}' created successfully.")
+        else:
+            print(f"Container '{container_name}' already exists.")
+
+        # Upload the blob
+        with open(file_path, "rb") as data:            
+            container_client.upload_blob(
+            name=blob_name,
+            data=data,
+            overwrite=True,
+            content_settings = ContentSettings(
+                content_type=file_type,
+                content_disposition="inline"
+                )
+            )
+
+            # print(f"Blob '{blob_name}' uploaded to container '{container_name}' successfully.")
+
+    except Exception as ex:
+        print(f"An error occurred: {ex}")
 
