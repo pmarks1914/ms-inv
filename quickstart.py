51c6adc20df27fe03495f19ca69dc8a274f2b296
Patrick <pmarks1914@gmail.com>
2025-01-22 22:28:09 +0000

    ai test

---
+++ quickstart.py
@@ -0,0 +1,16 @@
+import os
+from openai import AzureOpenAI
+
+
+
+response = client.chat.completions.create(
+    model=model, # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure AI services support this too?"}
+    ]
+)
+
+print(response.choices[0].message.content)
