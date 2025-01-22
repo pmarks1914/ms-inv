13c7dcde17d717f7915c8331968c2aabf1ead2bd
Patrick <pmarks1914@gmail.com>
2025-01-22 23:01:55 +0000

    reset password fix

--- quickstart.py
+++ quickstart.py
@@ -1,22 +1,28 @@
-import os
 from openai import AzureOpenAI
+from dotenv import dotenv_values
+
+get_env = dotenv_values(".env") 
+
+model=get_env["AZURE_OPENAI_MODEL"]
+
 
 client = AzureOpenAI(
-  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
-  api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-  api_version=os.getenv("AZURE_OPENAI_VERSION")
+  azure_endpoint = get_env["AZURE_OPENAI_ENDPOINT"], 
+  api_key=get_env["AZURE_OPENAI_API_KEY"],  
+  api_version=get_env["AZURE_OPENAI_VERSION"]
 )
 
 
-model=os.getenv("AZURE_OPENAI_MODEL")
 
 response = client.chat.completions.create(
     model=model, # model = "deployment_name".
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
-        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
-        {"role": "user", "content": "Do other Azure AI services support this too?"}
+        # {"role": "system", "content": "You are a helpful assistant."},
+        # {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        # {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        # {"role": "user", "content": "Do other Azure AI services support this too?"}
+
+        {"role": "user", "content": "tell me about the old Ghana empire"},
     ]
 )
 
